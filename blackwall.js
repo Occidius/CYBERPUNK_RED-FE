4c479d6f468703c914abe61bb2d625442f930346
Occidius <mattnayan546@gmail.com>
2026-02-18 12:27:44 -0800

    Added CSS customization, reformatting
    Reformatted Rogues section
    Reformatted all empty NPC sections
    Removed redundant/convoluted sectioning
    Ready to Copy/paste for other NPC's
    Added up-to-date Corner info for Rogue only

--- blackwall.js
+++ blackwall.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const RCHeader = document.getElementById('h1');
     RCHeader.style.textAlign = 'center';
     sizeAdjust(window.innerWidth, window.innerHeight)
+    document.body.style.backgroundColor = `rgb(${100} ${100} ${100})`;
+    
 })
 
 window.addEventListener('resize', () => {
@@ -21,7 +23,21 @@ function sizeAdjust(currWidth, currHeight)
         rightTarget[index].style.display = 'inline-block';
         leftTarget[index].style.float = 'left';
         rightTarget[index].style.float = 'right';
+        leftTarget[index].contentEditable = 'false';
+        rightTarget[index].contentEditable = 'false';
         leftTarget[index].style.width = ([window.innerWidth/2] - 70) + 'px';
+        leftTarget[index].style.height = '600px';
+        rightTarget[index].style.height = '600px';
         rightTarget[index].style.width = ([window.innerWidth/2] - 70) + 'px';
+    }
+
+    const image = document.getElementsByName('image');
+    for (imgIndex = 0; imgIndex < image.length; imgIndex++)
+    {
+        image[imgIndex].style.width = '100px';
+        image[imgIndex].style.height = '100px';
+        image[imgIndex].style.display = 'inline-block';
+        image[imgIndex].style.float = 'right';
+        image[imgIndex].isContentEditable = 'false';
     }
 }
