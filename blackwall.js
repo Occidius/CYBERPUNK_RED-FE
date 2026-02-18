67107a1b75a4b9141f823ade749414fed71c61f3
Occidius <mattnayan546@gmail.com>
2026-02-17 17:37:57 -0800

    Testing commit #2
    See previous

--- blackwall.js
+++ blackwall.js
@@ -1,5 +1,4 @@
-function init()
-{
-    const RCHeader = document.getElementById("h1");
-    RCHeader.style.textAlign = "center";
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const RCHeader = document.getElementById('h1');
+    RCHeader.style.textAlign = 'center';
+})
