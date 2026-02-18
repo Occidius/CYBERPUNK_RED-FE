eb80b0505a59bbabaa45d848f6ae6389aa8f434a
Occidius <mattnayan546@gmail.com>
2026-02-18 14:37:21 -0800

    Updates + news
    Added skeleton for newBulletin, updates to CSS, JS, and HTML for all NPC data and images

--- blackwall.js
+++ blackwall.js
@@ -3,7 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     RCHeader.style.textAlign = 'center';
     sizeAdjust(window.innerWidth, window.innerHeight)
     document.body.style.backgroundColor = `rgb(${100} ${100} ${100})`;
-    
+    document.body.style.backgroundImage = 'url(images/RCBACKGROUND.GIF)';
+
+    headerInit();
 })
 
 window.addEventListener('resize', () => {
@@ -25,10 +27,10 @@ function sizeAdjust(currWidth, currHeight)
         rightTarget[index].style.float = 'right';
         leftTarget[index].contentEditable = 'false';
         rightTarget[index].contentEditable = 'false';
-        leftTarget[index].style.width = ([window.innerWidth/2] - 70) + 'px';
-        leftTarget[index].style.height = '600px';
-        rightTarget[index].style.height = '600px';
-        rightTarget[index].style.width = ([window.innerWidth/2] - 70) + 'px';
+        leftTarget[index].style.width = ([window.innerWidth/2] - 60) + 'px';
+        leftTarget[index].style.height = '700px';
+        rightTarget[index].style.height = '700px';
+        rightTarget[index].style.width = ([window.innerWidth/2] - 60) + 'px';
     }
 
     const image = document.getElementsByName('image');
@@ -40,4 +42,11 @@ function sizeAdjust(currWidth, currHeight)
         image[imgIndex].style.float = 'right';
         image[imgIndex].isContentEditable = 'false';
     }
+}
+
+function headerInit()
+{
+    const RCHEADER = document.getElementById("h1");
+    // RCHEADER.style.backgroundColor = `rgb(${120} ${90} ${80})`;
+    RCHEADER.style.backgroundImage = `url(images/RCHEADER.jpg)`;    
 }
