d8dc40a1a14a2ff11300333bbd247c2b3aa672e6
Occidius <mattnayan546@gmail.com>
2026-02-18 21:06:33 -0800

    New HTMLs, CSS, JS updates
    New navigation code, nav menu, hamburger functionality, background formatting, initial commit for most new functionalities and navigation

--- blackwall.js
+++ blackwall.js
@@ -2,9 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const RCHeader = document.getElementById('h1');
     RCHeader.style.textAlign = 'center';
     sizeAdjust(window.innerWidth, window.innerHeight)
-    //document.body.style.backgroundColor = `rgb(${100} ${100} ${100})`;
-    //document.body.style.backgroundImage = `url(images/RCBACKGROUND.GIF)`;
-
+    document.body.style.backgroundColor = `rgb(${100} ${100} ${100})`;
+    // document.body.style.backgroundImage = `url(images/RCBACKGROUND.GIF)`;
+    const navMenu = document.getElementById('hiddenMenu');
+    navMenu.style.borderStyle = 'hidden';
     headerInit();
 })
 
@@ -28,8 +29,8 @@ function sizeAdjust(currWidth, currHeight)
         leftTarget[index].contentEditable = 'false';
         rightTarget[index].contentEditable = 'false';
         leftTarget[index].style.width = ([window.innerWidth/2] - 60) + 'px';
-        leftTarget[index].style.height = '750px';
-        rightTarget[index].style.height = '750px';
+        leftTarget[index].style.height = '800px';
+        rightTarget[index].style.height = '800px';
         rightTarget[index].style.width = ([window.innerWidth/2] - 60) + 'px';
     }
 
@@ -47,9 +48,34 @@ function sizeAdjust(currWidth, currHeight)
 function headerInit()
 {
     const RCHEADER = document.getElementById("h1");
+    const HAMBURG = document.getElementById("burgButton");
     // RCHEADER.style.backgroundColor = `rgb(${120} ${90} ${80})`;
-    RCHEADER.style.backgroundImage = `url(images/RCHEADER.jpg)`;    
-
+    RCHEADER.style.backgroundImage = `url(images/RCHEADER.jpg)`;  
+    HAMBURG.style.backgroundImage = `url(image/HAMBURG.png)`;
+}
+function loadPage(destination)
+{
+    // alert("CALLED, SEND TO: " + destination);
+    window.location.href = destination;
 }
 
+function openMenu()
+{
+    // alert("CALLED NEWS OPENER");
+    const navMenu = document.getElementById('hiddenMenu');
+    const navFS = document.getElementById('navFS');
+    navMenu.style.width = '250px';
+    navMenu.style.borderStyle = 'ridge';
+    // navFS.style.backgroundImage = `url(images/NAVBACKGROUND.gif)`;
+    // document.getElementById('ad').style.width = '20px';
+    // document.getElementById('ad').style.height = '20px';
+    // navMenu.style.marginLeft = '250px';
+    // navMenu.style.alignmentBaseline = 'left';
+}
 
+function closeMenu()
+{
+    const navMenu = document.getElementById('hiddenMenu');
+    navMenu.style.width = '0px';
+    navMenu.style.borderStyle = 'hidden';
+}
