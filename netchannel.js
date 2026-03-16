e02268b45e24d7c03aaaf6f8ba9451b1ef82dcb1
Occidius <mattnayan546@gmail.com>
2026-03-15 18:04:31 -0700

    Session 21 Data
    Update player QM's and new Job listings

--- netchannel.js
+++ netchannel.js
@@ -105,14 +105,14 @@ function ROOK()
 function POPULATEAQM()
 {
     const QMTARGET = document.getElementsByClassName('QMItem');
-    PCQUEST1 = "Vigilante Shit:\n-CONTINUE MAIN CAMPAIGN FOR UPDATES\n\n";
-    PCQUEST2 = "Anti-Hero:\n-Arasaka bastards in Japantown and now someone\'s wearing my face, messing with my network? Shits getting deep...hope you got some decent boots...\n-CONTINUE MAIN CAMPAIGN FOR UPDATES\n\n";
+    PCQUEST1 = "Vigilante Shit:\n-BIG SIS MICHIKO!\n=> Succesfully infiltrate the Arasaka Datavault Alt pin-pointed and upload her internal gateway\n\n";
+    PCQUEST2 = "Anti-Hero:\n-Well, atleast now we know how Arasaka picked up my trail again, fuckin Ghoul...\n=> Use Alt's help to get out of Arasaka sight-lines\n\n";
     PCQUEST3 = "[HELLA] - New American Idiot:\n-IN-PROGRESS [Crews are refurbishing the Pigs Wing]\n";
     PCQUEST4 = "UNRECOVERABLES: WATSON ADDRESS\nUNRECOVERABLES: CORPO PLAZA ADDRESS\nUNRECOVERABLES: WESTBROOK ADDRESS\n";
     QMTARGET[PCQUEST].textContent = PCQUEST1 + PCQUEST2 + PCQUEST3 + PCQUEST4;   
 
-    JOBTASK1 = "Junkyard Warehouse - Primo: Weekly 7% finders fee(X) \n^^^[Traded for reduced rent (1200 EB)]\n-Sold Tiger Skeleton to Reginald Ketel\nCalus's Lockbox:\n-[3] Electronic Locks\n-[2] Brass Padlocks\n-[1] Broken Biometric Reader\n"; 
-    JOBTASK2 = "Wait for a Ping from Viktor\n"; 
+    JOBTASK1 = "=> Meet with Viktor in Santo Domingo\n[OPTIONAL: Bring Hella, Valve, and/or Sammie\n"; 
+    JOBTASK2 = "Calus's Lockbox:\n-[3] Electronic Locks\n-[2] Brass Padlocks\n-[1] Broken Biometric Reader\n";     
     JOBTASKF = "NEPHEW: Payout processing: Estimated for November 6th\nPRIMO:N/A\nSJ: Keeping tabs on public outlook on Hella's show\nALT: N/A\nSiobhan:N/A\n";
     JOBTASK3 = "Date Night: The date with Mito went...kind of well? He's sweet, and a little scatter-brained - Wait for Mito to reach out\n";
     JOBTASK4 = "Tequila Payout: 2250 EB [SET TO CLOSE IN {3} DAYS]\n";
@@ -126,11 +126,11 @@ function POPULATEAQM()
 
 function POPULATEHQM(){
     const QMTARGET = document.getElementsByClassName('QMItem');
-    PCQuest1 = "Sucka Free:\n-Mr. Hands mentioned everyone in the crew playing their role, and if someone else in the crew can play the leader: I can play my part to a sweeter tune...wonder what that means...\n-CONTINUE MAIN QUEST FOR UPDATES\n\n";
-    PCQuest2 = "New American Idiot:\n-Prep for the show at the Pigs Wing tomorrow\n\n";
+    PCQuest1 = "Sucka Free:\n-Alright, lets play the role of pain-in-Arasakas-Ass\n=> Help Ace and the crew get Alt Cunningham into Arasaka's Datavault\n\n";
+    PCQuest2 = "New American Idiot:\n-Check in with Mito for the final preparations\n\n";
     QMTARGET[PCQUEST].textContent = PCQuest1 + PCQuest2;
 
-    JOBTASK1 = "N.A.I. : 0/16,000 EB\n";
+    JOBTASK1 = "N.A.I. : 2,000 /16,000 EB\n";
     JOBTASK2 = "";
     JOBTASK3 = ""; 
     QMTARGET[JOBTASK].textContent = JOBTASK1 + JOBTASK2 + JOBTASK3;
@@ -142,7 +142,8 @@ function POPULATEHQM(){
 }
 function POPULATEVQM(){
     const QMTARGET = document.getElementsByClassName('QMItem');
-    PCQuest1 = "Dirty Harry:\n-Mallard knows his weapons! We figured out that much about his past after he managed to somehow fix Boogaloo's jamming issue. Man....all these veterans across NC should really buddy-up or somethin...\n-CONTINUE MAIN CAMPAIGN FOR UPDATES\n\n";
+    PCQuest1 = "Dirty Harry:\n.....His name is Tobias??\n=> Investigate what happened with Mallard's Eye\n";
+    PCQuestE = "ALTERNATIVE: Help Mallard access the report Michiko gave him\n\n";
     PCQuest2 = "BOOGALOO COMPONENTS:\n-WEAPONS TECH: 149\n-HIGH-GRADE MATS: 35\n";
     QMTARGET[PCQUEST].textContent = PCQuest1 + PCQuest2;
 
@@ -161,9 +162,10 @@ function POPULATEVQM(){
 }
 function POPULATERQM(){
     const QMTARGET = document.getElementsByClassName('QMItem');
-    PCQuest1 = "I Did and I Don't and I Do:\n........fuck.....\nESCAPE THE ICARUS LAB\n\n";
+    PCQuest1 = "I Did and I Don't and I Do:\nOkay, so we found Reece and now I have to sit still while Foster gets sent down to Icarus....FUCK\n=> Help Hella use her S.D. show to expose Icarus\n\n";
+    PCQuestE = "Anti-Hero [ACE]\n=> Help the crew get Alt Cunningham access to the Arasaka Datavault\n\n";
     PCQuest2 = "But Once A Child:\n-Okay....we're making some progress!\n\n-Netrunner Gear: 0 / 2K\n-Weapons: 0 / 250 EB\n-Armor: 0 / 100 EB\n-Cyberware 0 / 5K\n-Misc Services: 0 / 5K\n-Housing: 8.5K / 40K\n-Find new Housing Option\n";
-    QMTARGET[PCQUEST].textContent = PCQuest1 + PCQuest2;
+    QMTARGET[PCQUEST].textContent = PCQuest1 + PCQuestE + PCQuest2;
 
     JOBTASK1 = "See Nothin\', Say Nothin(2)\'\n-Wait for Nyx to contact you.\n"; 
     JOBTASK2 = ""; 
@@ -182,11 +184,8 @@ function COMMONQM()
     
     const CMTARGET = document.getElementsByClassName('CommonItem');
 
-    CMTARGET[CAMPAIGN].textContent = "[PRIMARY] = Understanding a Ghost [Rogue] - Rendevouz with Torch\n[SAMMIE] = ESCAPE THE MILITECH LAB\n";
-
+    CMTARGET[CAMPAIGN].textContent = "[PRIMARY] => Enter Sandman [Michiko] - Meet with the Aldecaldos in the Badlands\n";
     NPCQUEST1 = "Never Fade Away - Torch\n-Await an update on the situation\n";
     NPCQUEST2 = "Tech Drops - Torch\n-Investigate the safehouse locations Torch sent your crew\n";
-    NPCQUEST3 = "Goin' On Patrol - El Capitan\n-Wait for updates from the Capitan about Sarah's project\n";
-    CMTARGET[NPCQUEST].textContent = NPCQUEST1 + NPCQUEST2 + NPCQUEST3;
-
+    CMTARGET[NPCQUEST].textContent = NPCQUEST1 + NPCQUEST2;
 } 
