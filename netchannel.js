6ed8592f8448f930b55db8b2d34df98cc9859288
Occidius <mattnayan546@gmail.com>
2026-07-29 14:47:36 -0700

    SESSION 27 Data
    After two longer hiatuses, includes new quest info, reformatting for some, and updates to Valve's available materials

--- netchannel.js
+++ netchannel.js
@@ -105,17 +105,17 @@ function ROOK()
 function POPULATEAQM()
 {
     const QMTARGET = document.getElementsByClassName('QMItem');
-    PCQUEST1 = "Vigilante Shit:\n-BIG SIS MICHIKO!\n=> Succesfully infiltrate the Arasaka Datavault Alt pin-pointed and upload her internal gateway\n\n";
-    PCQUEST2 = "Anti-Hero:\n-With the Ghoul trapped under the Icarus rubble, we can safely assume most if not EVERYONE we talk to for the next few days to be the real-deal.\n=> Use Alt's help to get out of Arasaka sight-lines\n\n";
+    PCQUEST1 = "Vigilante Shit:\n-Datavault succesfully chipped!\n=> Wait for Alt to contact you\n\n";
+    PCQUEST2 = "Anti-Hero:\n-With the Ghoul trapped under the Icarus rubble, now's a good time to hunt down some of his lieutenants\n=> Talk to Nyx about dealing with Aryx, the Ghoul's Netrunner\n\n";
     PCQUEST3 = "";
-    PCQUEST4 = "UNRECOVERABLES: WATSON ADDRESS\nUNRECOVERABLES: CORPO PLAZA ADDRESS\nUNRECOVERABLES: WESTBROOK ADDRESS\n";
+    PCQUEST4 = "UNRECOVERABLES: Wait for Amelia to return your call\n";
     QMTARGET[PCQUEST].textContent = PCQUEST1 + PCQUEST2 + PCQUEST3 + PCQUEST4;   
 
     JOBTASK1 = ""; 
     JOBTASK2 = "Calus's Lockbox:\n-[3] Electronic Locks\n-[2] Brass Padlocks\n-[1] Broken Biometric Reader\n";     
-    JOBTASKF = "NEPHEW: Payout processing: Estimated for November 6th\nPRIMO:N/A\nSJ:N/A\nALT: N/A\nSiobhan:N/A\n";
+    JOBTASKF = "NEPHEW: N/A\nPRIMO:N/A\nSJ:N/A\nALT: N/A\nSiobhan:N/A\n";
     JOBTASK3 = "Date Night: The date with Mito went...kind of well? He's sweet, and a little scatter-brained - Wait for Mito to reach out\n";
-    JOBTASK4 = "Tequila Payout: 2250 EB [SET TO CLOSE IN {1} DAY]\n";
+    JOBTASK4 = "\n";
     QMTARGET[JOBTASK].textContent = JOBTASK1 + JOBTASK2 + JOBTASK3 + JOBTASK4 + JOBTASKF;
     
     MINMAJ1 = "";
@@ -126,53 +126,52 @@ function POPULATEAQM()
 
 function POPULATEHQM(){
     const QMTARGET = document.getElementsByClassName('QMItem');
-    PCQuest1 = "Sucka Free:\n-Alright, lets play the role of pain-in-Arasakas-Ass\n=> Help Ace and the crew get Alt Cunningham into Arasaka's Datavault\n\n";
-    PCQuest2 = "New American Idiot:\n-Now THAT's how you bring down a secret laboratory\n=> Deal with the aftermath of the raid, Rendezvouz with Kyson, and deal with Saint Providence\n\n";
+    PCQuest1 = "Sucka Free:\n-Fuck ur datavault, and FUCK ur supervising officer\n=> Keep an eye out for objectives from Lucius and Hornet\n\n";
+    PCQuest2 = "New American Idiot:\n-\n=> Continue Main Campaign\n\n";
     QMTARGET[PCQUEST].textContent = PCQuest1 + PCQuest2;
 
-    JOBTASK1 = "N.A.I. : 2,000 /16,000 EB\n";
+    JOBTASK1 = "N/A\n";
     JOBTASK2 = "";
-    JOBTASK3 = ""; 
+    JOBTASK3 = "\n"; 
     QMTARGET[JOBTASK].textContent = JOBTASK1 + JOBTASK2 + JOBTASK3;
 
-    MINMAJ1 = "";
+    MINMAJ1 = "N/A";
     MINMAJ2 = "";
 
     QMTARGET[MAJORMINOR].textContent = MINMAJ1 + MINMAJ2;
 }
 function POPULATEVQM(){
     const QMTARGET = document.getElementsByClassName('QMItem');
-    PCQuest1 = "Dirty Harry:\nDad's name was Teddy, or 'Theodore'.....The video mentioned the Valentinos\n=> Find a way to contact the Valentinos in Heywood\n";
-    PCQuestE = "ALTERNATIVE: Help Mallard access the report Michiko gave him\n\n";
-    PCQuest2 = "BOOGALOO COMPONENTS:\n-WEAPONS TECH: 149\n-HIGH-GRADE MATS: 35\n";
+    PCQuest1 = "Dirty Harry:\n-That was weird...\n=> Visit the location on the chip Padre gave you\n";
+    PCQuestE = "OPTIONAL: Help Mallard access the report Michiko gave him\n\n";
+    PCQuest2 = "BOOGALOO COMPONENTS:\n-WEAPONS TECH: 53\n-HIGH-GRADE MATS: 7\n";
     QMTARGET[PCQUEST].textContent = PCQuest1 + PCQuestE+ PCQuest2;
 
     JOBTASK1 = "Amalgamated Standard Hand: 0%\n"; 
     JOBTASK2 = "Turtle Kings Mantle: COMPLETED\n"; 
     JOBTASK3 = "Rocket Powered Heely's: COMPLETED\n";
     JOBTASK4 = "Surprise C4: COMPLETED\n";
-    JOBTASK5 = "MUNCHIES: 0%\n";
+    JOBTASK5 = "MUNCHIES: COMPLETED\n";
     JOBTASK6 = "ALT F4: COMPLETED\n";
     QMTARGET[JOBTASK].textContent = JOBTASK1 + JOBTASK2 + JOBTASK3 + JOBTASK4 + JOBTASK5 + JOBTASK6;
 
-    MINMAJ1 = "";
+    MINMAJ1 = "N/A";
     MINMAJ2 = "";
 
     QMTARGET[MAJORMINOR].textContent = MINMAJ1 + MINMAJ2;
 }
 function POPULATERQM(){
     const QMTARGET = document.getElementsByClassName('QMItem');
-    PCQuest1 = "I Did and I Don't and I Do:\nGET FUCKED SAINT, YOU RIGHTEOUS PRICK\n=> Find a way to deal with Saint Providence.\n\n";
-    PCQuestE = "Anti-Hero [ACE]\n=> Help the crew get Alt Cunningham access to the Arasaka Datavault\n\n";
-    PCQuest2 = "But Once A Child:\n-Okay....we're making some progress!\n\n-Netrunner Gear: 0 / 2K\n-Weapons: 0 / 250 EB\n-Armor: 0 / 100 EB\n-Cyberware 0 / 5K\n-Misc Services: 0 / 5K\n-Housing: 8.5K / 40K\n-Find new Housing Option\n";
-    QMTARGET[PCQUEST].textContent = PCQuest1 + PCQuestE + PCQuest2;
+    PCQuest1 = "I Did and I Don't and I Do:\nGET FUCKED SAINT, YOU RIGHTEOUS PRICK\n=> Open the datachip Kathleen gave you\n\n";
+    PCQuest2 = "But Once A Child:\n-Goodbye crippling debt, HELLOOOOOO ONE HUNDRED THOUSAND EDDIES\n=> QUEST COMPLETE\n";
+    QMTARGET[PCQUEST].textContent = PCQuest1 + PCQuest2;
 
-    JOBTASK1 = "See Nothin\', Say Nothin(2)\'\n-Respond to Nyx's new Ping\n"; 
+    JOBTASK1 = ""; 
     JOBTASK2 = ""; 
     JOBTASK3 = "";
     QMTARGET[JOBTASK].textContent = JOBTASK1 + JOBTASK2 + JOBTASK3;
 
-    MINMAJ1 = "";
+    MINMAJ1 = "N/A";
     MINMAJ2 = "";
     MINMAJ3 = "";
 
@@ -184,10 +183,10 @@ function COMMONQM()
     
     const CMTARGET = document.getElementsByClassName('CommonItem');
 
-    CMTARGET[CAMPAIGN].textContent = "[PRIMARY] => Enter Sandman [Rogue] - Meet with the Aldecaldos in the Badlands\n[PRIMARY] => Suspicious Minds [Michiko] - Find Kenshiro Saeba, former NCPD Lieutenant\n[SAMMIE] => GREYLOCK - Find a way to deal with Saint Providence / Rendezvouz with Hella\n";
+    CMTARGET[CAMPAIGN].textContent = "[PRIMARY] => Enter Sandman [Rogue] - Meet with the Aldecaldos in the Badlands\n[PRIMARY] => Suspicious Minds [Michiko] - Find Kenshiro Saeba, former NCPD Lieutenant\n";
     NPCQUEST1 = "Never Fade Away - Torch\n-Respond to Torch's Ping regarding Elly\n\n";
     NPCQUEST2 = "Tech Drops - Torch\n-Investigate the safehouse locations Torch sent your crew\n\n";
-    NPCQUEST3 = "Ghost In The Shell - Nyx\n-Respond to Nyx's Ping\n\n";
-    NPCQUEST4 = "Big Brohter - El Capitan\n-Check in with El Capitan to see what he found using Sarah's Network.\n\n";
-    CMTARGET[NPCQUEST].textContent = NPCQUEST1 + NPCQUEST2 + NPCQUEST3;
+    NPCQUEST3 = "Ghost In The Shell - Nyx\n-Talk with Nyx to begin trapping the Ghoul's Netrunner\n\n";
+    NPCQUEST4 = "Big Brother - El Capitan\n-Check in with El Capitan to see what he found using Sarah's Network.\n\n";
+    CMTARGET[NPCQUEST].textContent = NPCQUEST1 + NPCQUEST2 + NPCQUEST3 + NPCQUEST4;
 } 
